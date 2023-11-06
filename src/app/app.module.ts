5b1d4622f7c039c2aa68bde070453db1cc7ce685
thi4go-git <thi4go19@gmail.com>
2023-11-06 13:51:04 -0300

    Ajustes layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/template/header/header.component';
+import { MenuComponent } from './components/template/menu/menu.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
